acc0ccecbc86ae48383893236c27537e7b6ea79c
rnegic <arnego666@gmail.com>
2025-03-31 18:40:38 +0300

    Feat/Added configs for vite testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
