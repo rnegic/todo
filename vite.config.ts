967ef9630ea4df7b3dddb5fd921455f788fa1df8
rnegic <arnego666@gmail.com>
2025-04-01 00:09:07 +0300

    Fix/gh pages related configs updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todo/",
   test: {
     globals: true,
     environment: 'jsdom',
